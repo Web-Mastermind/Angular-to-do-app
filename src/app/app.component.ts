28261e0c43575c84d74dce781422d1b9966fc2ca
Web-Mastermind <AZEAgent@gmail.com>
2024-07-03 00:57:26 +0400

    added update button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,23 +7,55 @@ import { FormsModule } from '@angular/forms';
   imports: [FormsModule],
   template: `
   <h1>Todo App</h1>
-  <div>
-    <label>Work</label>
-    <input [(ngModel)]="work">
-    <button (click)="save()">Save</button>
+  @if(!isUpdateWorkFormActive){
+    <div>
+      <label>Work</label>
+      <input [(ngModel)]="work">
+      <button (click)="save()">Save</button>
+    </div>
+  }@else {
+    <div>
+      <label>Update Work</label>
+      <input [(ngModel)]="updateWork">
+      <button (click)="update()">Update</button>
   </div>
+  }
   <hr>
   <div>
     <ul>
-      <li></li>
+      @for(data of todos; track data){
+        <li>{{data}}
+          @if(!isUpdateWorkFormActive){
+            <button (click)="get($index)">Update</button>
+            <button (click)="Delete($index)">Delete</button>
+          }
+        </li>
+      }
     </ul>
   </div>
 `
 })
 export class AppComponent {
   work: string = "";
-  todos: string[] = []
+  updateWork: string = "";
+  updateIndex: number = 0;
+  todos: string[] = [];
+  isUpdateWorkFormActive: boolean = false;
+
   save() {
-    this.todos.push(this.work)
+    this.todos.push(this.work);
+    this.work = "";
+  }
+  Delete(index: number) {
+    this.todos.splice(index, 1)
+  }
+  get(index: number) {
+    this.updateIndex = index;
+    this.updateWork = this.todos[index]
+    this.isUpdateWorkFormActive = true
+  }
+  update() {
+    this.todos[this.updateIndex] = this.updateWork
+    this.isUpdateWorkFormActive = false
   }
 }
